3d538e53f21869640e8bb307ef65db96e03b6c5a
Lixin Duan <lixindu@amazon.com>
2015-04-03 17:58:24 -0700

    Modify Examples class to allow to take an argument of the path of an image file

--- src/Examples.java
+++ src/Examples.java
@@ -5,16 +5,23 @@ import io.korhner.asciimg.image.character_fit_strategy.StructuralSimilarityFitSt
 import io.korhner.asciimg.image.converter.AsciiToImageConverter;
 import io.korhner.asciimg.image.converter.AsciiToStringConverter;
 
-import java.awt.Font;
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-import javax.imageio.ImageIO;
-
 public class Examples {
 
 	public static void main(String[] args) throws IOException {
+		if (args.length != 1) {
+			System.out.println("Only one argument is allowed");
+			System.out.println("Usage:");
+			System.out.println("    java " + Examples.class.getName() + " imagePath");
+			System.exit(0);
+		}
+		String imagePath = new File(args[0]).getAbsolutePath();
+		System.out.println("Working on: " + imagePath);
 
 		// initialize caches
 		AsciiImgCache smallFontCache = AsciiImgCache.create(new Font("Courier",
@@ -24,9 +31,12 @@ public class Examples {
 		AsciiImgCache largeFontCache = AsciiImgCache.create(new Font("Courier",
 				Font.PLAIN, 16));
 
+		String[] parts = imagePath.split("/");
+		String fileName = parts[parts.length-1].split("\\.")[0];
+		String saveDir = new File(args[0]).getParent();
+
 		// load image
-		BufferedImage portraitImage = ImageIO.read(new File(
-				"examples/portrait.png"));
+		BufferedImage portraitImage = ImageIO.read(new File(imagePath));
 
 		// initialize algorithms
 		BestCharacterFitStrategy squareErrorStrategy = new ColorSquareErrorFitStrategy();
@@ -42,40 +52,40 @@ public class Examples {
 		imageConverter.setCharacterCache(smallFontCache);
 		imageConverter.setCharacterFitStrategy(squareErrorStrategy);
 		ImageIO.write(imageConverter.convertImage(portraitImage), "png",
-				new File("examples/portrait_small_square_error.png"));
+				new File(saveDir + "/" + fileName + "_portrait_small_square_error.png"));
 
 		// medium font images, square error
 		imageConverter.setCharacterCache(mediumBlackAndWhiteCache);
 		imageConverter.setCharacterFitStrategy(squareErrorStrategy);
 		ImageIO.write(imageConverter.convertImage(portraitImage), "png",
-				new File("examples/portrait_medium_square_error.png"));
+				new File(saveDir + "/" + fileName + "_portrait_medium_square_error.png"));
 
 		// large font images, square error
 		imageConverter.setCharacterCache(largeFontCache);
 		imageConverter.setCharacterFitStrategy(squareErrorStrategy);
 		ImageIO.write(imageConverter.convertImage(portraitImage), "png",
-				new File("examples/portrait_large_square_error.png"));
+				new File(saveDir + "/" + fileName + "_portrait_large_square_error.png"));
 
 		// small font images, ssim
 		imageConverter.setCharacterCache(smallFontCache);
 		imageConverter.setCharacterFitStrategy(ssimStrategy);
 		ImageIO.write(imageConverter.convertImage(portraitImage), "png",
-				new File("examples/portrait_small_ssim.png"));
+				new File(saveDir + "/" + fileName + "_portrait_small_ssim.png"));
 
 		// medium font images, ssim error
 		imageConverter.setCharacterCache(mediumBlackAndWhiteCache);
 		imageConverter.setCharacterFitStrategy(ssimStrategy);
 		ImageIO.write(imageConverter.convertImage(portraitImage), "png",
-				new File("examples/portrait_medium_ssim.png"));
+				new File(saveDir + "/" + fileName + "_portrait_medium_ssim.png"));
 
 		// large font images, ssim
 		imageConverter.setCharacterCache(largeFontCache);
 		imageConverter.setCharacterFitStrategy(ssimStrategy);
 		ImageIO.write(imageConverter.convertImage(portraitImage), "png",
-				new File("examples/portrait_large_ssim.png"));
+				new File(saveDir + "/" + fileName + "_portrait_large_ssim.png"));
 
 		// string converter, output to console
-		System.out.println(stringConverter.convertImage(portraitImage));
-
+//		System.out.println(stringConverter.convertImage(portraitImage));
+		System.out.println("* done! *");
 	}
 }
